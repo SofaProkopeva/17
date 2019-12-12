48ed9f94e7aea4b1d0a0ccda907bf78d2ac45564
SofaProkopeva <55788579+SofaProkopeva@users.noreply.github.com>
2019-12-12 21:35:45 +0300

    Add files via upload

---
+++ прогр 17.1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n, i, j, t, k, l, buf;
+	cout << "Введите размер массива N: ";
+	cin >> n;
+	int* a = new int[n];
+	int* b = new int[n];
+	int* c = new int[n];
+	t = l = k = 0;
+	cout << "Введите элементы массива: ";
+	for (i = 0; i < n; i++)
+		cin >> a[i];
+	for (i = l; i < n; i++)
+	{
+		if (a[i] == a[i + 1])
+			t++;
+		else
+		{
+			l = t;
+			buf = a[i];
+			l ++;
+			b[k] = l;
+			c[k] = buf;
+			k ++;
+			t = 0;
+		}
+	}
+	cout << "Массив из серий:" << endl;
+	for (int i = 0; i < k; i++)
+		cout << b[i] << " ";
+	cout << endl;
+	cout << "Массив из элементов:" << endl;
+	for (int i = 0; i < k; i++)
+		cout << c[i] << " ";
+}
---
+++ прогр 17.2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	int n, i, j, t, k, l, L, buf;
+	cout << "N=";
+	cin >> n;
+	cout << "L=";
+	cin >> L;
+	int* a = new int[n];
+	int* b = new int[n];
+	int* c = new int[n];
+	t = l = k = 0;
+	for (i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	for (i = l; i < n; i++)
+	{
+		if (a[i] == a[i + 1])
+			t ++;
+		else
+		{
+			l = t;
+			buf = a[i];
+			l ++;
+			b[k] = l;
+			c[k] = buf;
+			k ++;
+			t = 0;
+		}
+	}
+	for (i = 0; i < k; i++)
+		if (b[i] > L)
+			c[i] = 0;
+	for (int i = 0; i < k; i++)
+		cout << c[i] << " ";
+}
